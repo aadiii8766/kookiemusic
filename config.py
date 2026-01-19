c037635ea16a8df9babfe5c253f153a750ce409d
aadiii8766 <aadiii8766@gmail.com>
2026-01-20 02:35:35 +0530

    Update config.py
    Signed-off-by: aadiii8766 <aadiii8766@gmail.com>

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ OWNER_ID = int(getenv("8099478929"))
 PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
 START_IMG = getenv("START_IMG", "https://te.legra.ph/file/f8ba75bdbb9931cbc8229.jpg")
 
-SESSION = getenv("SESSION", None)
+SESSION = getenv("BQFXh-kAiMWf0aG_g5DH_laLRYPYQV6II-C8LRD_DHXQGp2ZgMdEV8CRnDPhr2DxT6xyJy5Lpxf8h8fSYCcCv7dzXjPrNooI6jjzq6E1HAoDchVn6rrWMhL0MhZmPkB-wFcKvEn0QBIqhf_-L4SlCgIx8_Bzv4jfy9pGKen2U1Bq9UQQ0RmxqM62vJPxMmTcsKc73gAgt3WZpJMrap_S3Z-nP2QOV-zPjLd1r5mmaxEPTr3vY_Hz9oXIMzZsWJBfs975BjHB7jiD8xboLvRZ6v7v0aMolEFQn4KYoaLderPMdRXa4TNQOKqZ5bEj6yRKb8jdPT4YHpKVe-WUSCiF4asK9Fjj8QAAAAHixD2RAA", None)
 
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+uGojToopWyJmNTZl")
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/+fLVvGqzz_6s4NmFl")
